615bbf702a9318960d352758dedd221e27a86283
cajotafer <hello@cajotafer.com>
2020-06-30 18:48:16 +0700

    chore(back): front build to back. Server serves /build

---
+++ mern-blog-back/src/build/precache-manifest.09cfa938bff77c8f86b7336caa761092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3620eed30dbd953ea2ac91880295674",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fb6c07492b103f9b2fe",
+    "url": "/static/css/main.d14035dc.chunk.css"
+  },
+  {
+    "revision": "f4a267c67adeff44317a",
+    "url": "/static/js/2.7cfcdddd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7cfcdddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb6c07492b103f9b2fe",
+    "url": "/static/js/main.00513557.chunk.js"
+  },
+  {
+    "revision": "f32fa013b6d79392388e",
+    "url": "/static/js/runtime-main.3edc87a7.js"
+  }
+]);
